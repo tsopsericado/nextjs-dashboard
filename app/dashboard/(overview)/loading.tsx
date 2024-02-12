4330268d6692298266d92d1384bab8dbeb7ddd9d
ricardo <ricardotsopse@gmail.com>
2024-02-12 16:49:42 +0100

    working on pagination

--- app/dashboard/(overview)/loading.tsx
+++ app/dashboard/(overview)/loading.tsx
@@ -2,4 +2,4 @@ import DashboardSkeleton from "../../ui/skeletons";
 
 export default function Loading() {
     return <DashboardSkeleton />
-}
+}   
--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,12 +2,12 @@
 //     return <div>uohpo</div>
 // }
 
-import { Card } from '@/app/ui/dashboard/cards';
+import CardWrapper, { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import { Suspense } from 'react';
-import { RevenueChartSkeleton } from '@/app/ui/skeletons';
+import { CardSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
 import {
   fetchLatestInvoices,
   fetchCardData,
@@ -28,7 +28,7 @@ export default async function Page() {
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Dashboard
       </h1>
-      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+      {/* <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
         <Card title="Collected" value={totalPaidInvoices} type="collected" />
         <Card title="Pending" value={totalPendingInvoices} type="pending" />
         <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
@@ -37,12 +37,13 @@ export default async function Page() {
           value={numberOfCustomers}
           type="customers"
         />
-      </div>
+      </div> */}
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <Suspense fallback={<RevenueChartSkeleton />}>
-          <RevenueChart />
+        <Suspense fallback={<CardSkeleton />}>
+          <CardWrapper />
+          {/* <RevenueChart /> */}
         </Suspense>
-        <LatestInvoices latestInvoices={latestInvoices} />
+        {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
       </div>
     </main>
   );
