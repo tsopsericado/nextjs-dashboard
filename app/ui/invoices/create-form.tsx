dc8bd516d4c47def6bde264f343e908fa4499cff
ricardo <ricardotsopse@gmail.com>
2024-02-17 16:49:03 +0100

    handling error in nextjs

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { CustomerField } from '@/app/lib/definitions';
 import Link from 'next/link';
 import {
@@ -7,10 +8,19 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
+// import { createInvoice } from '@/app/lib/actions';
+import { useFormState } from 'react-dom';
+import { createInvoice } from '@/app/lib/actions';
+
+
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
+  const initialState = { message: null, errors: {} };
+  const [state, dispatch] = useFormState(createInvoice, initialState);
+
+
   return (
-    <form>
+    <form action={dispatch}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
